f55aa8c7e4dd51c40b663177e49885787e52095f
faatemeh chamani <fch199674@yahoo.com>
2021-12-18 17:30:35 +0330

    updated the view

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request, *args, **kwargs):
+    context = {
+        'title': 'Dictionary App'
+    }
     if request.method == 'GET':
-        context = {
-            'title': 'Dictionary App'
-        }
+        return render( request, 'dictionary/index.html', context )
+    elif request.method == 'POST':
+        word = request.POST.get('word')
+        url = f'https://www.dictionary.com/browse/{word}'
+        response = requests.get( url=url )
         return render( request, 'dictionary/index.html', context )
