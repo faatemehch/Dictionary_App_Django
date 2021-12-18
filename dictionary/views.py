4bba7e043b41ba1f3519c4091eb0358123f534ce
faatemeh chamani <fch199674@yahoo.com>
2021-12-18 17:51:52 +0330

    updated the view

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+from bs4 import BeautifulSoup
 
 
 def index(request, *args, **kwargs):
@@ -9,7 +10,14 @@ def index(request, *args, **kwargs):
     if request.method == 'GET':
         return render( request, 'dictionary/index.html', context )
     elif request.method == 'POST':
-        word = request.POST.get('word')
+        word = request.POST.get( 'word' )
         url = f'https://www.dictionary.com/browse/{word}'
         response = requests.get( url=url )
+        # use BeautifulSoup to extract data from HTML
+        soup = BeautifulSoup( response.content, 'html.parser' )
+        # the definitions are in span tag so fine them
+        spans = soup.find_all( 'span', {"class": "one-click-content"} )
+        print( spans[0].text )
+        context['word'] = word
+        context['definition'] = spans[0].text
         return render( request, 'dictionary/index.html', context )
